07fbad3c94b56ad47bc080c06eadb25b5c1dce81
TheoFj <theo.feijoo@gmail.com>
2024-04-29 18:58:37 +0200

    Initialisation de mon super projet bonjour

---
+++ toto.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void affiche_grille(grid)
+{
+  for(i=0;i<3;i++)
+    {
+    for(f=0;i<3;i++)
+      printf("%c", grid[i][j]);
+    printf("\n");
+    }
+}
+
+int main(void)
+{
+  printf("toto");
+}
